910ee2419860c820caa96b2b98b36e341664e06a
sofia alcaraz <sofiialcaraz16@gmail.com>
2023-04-02 19:12:06 -0300

    se agrego errores

--- src/Page/Login.js
+++ src/Page/Login.js
@@ -13,11 +13,24 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+
 import Webcam from 'react-webcam';
 import { useCallback, useRef, useState } from "react";
+import { Modal } from '@mui/material';
 
 
 const theme = createTheme();
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 
 const videoConstraints = {
@@ -31,7 +44,8 @@ export default function Login() {
   const [imgSrc, setImgSrc] = useState(null);
   const webcamRef = useRef(null);
   const [user, setUser] = useState(null);
-
+  const [openModal, setOpenModal] = React.useState(false);
+const [botton,setBotton]=useState(true);
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current.getScreenshot();
     setImgSrc(imageSrc);
@@ -57,98 +71,127 @@ export default function Login() {
       })
     };
     fetch('https://k9lcx9c6n9.execute-api.us-east-1.amazonaws.com/dev/user', requestOptions)
-      .then(response => response.json())
-      .then(data => setUser(data));
+      .then(response => {
+        if(response.status==403){
+          setOpenModal(true)
+          return;          
+        }
+        response.json().then((data) => {
+          setUser(data); window.location.replace("/home")  
+        })
+      })
   };
-  if (user) {
-    window.location.replace("/home");
-  }
+
+
 
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Login
+    <div>
+      <Modal
+        open={openModal}
+        onClose={() => setOpenModal(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            No existe la cuenta ingresada
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Direccion de Email"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-
-                  fullWidth
-                  name="password"
-                  label="Contraseña"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-              <div style={{ display: 'flex', margin: '5px' }}>
-                                    {(!imgSrc) ? <Webcam
-                                        audio={false}
-                                        screenshotFormat="image/jpeg"
-                                        ref={webcamRef}
-                                        videoConstraints={videoConstraints}
-                                    >
-                                    </Webcam> :
-                                        (
-                                            <img style={{ margin: '5px' }}
-                                                src={imgSrc}
-                                            />
-                                        )
-                                    }
-                                </div>
-
-                                {imgSrc ? <Button color={'error'} onClick={() => setImgSrc(null)} s startIcon={<DeleteIcon />}>
-                                    Descartar foto
-                                </Button> : <Button onClick={capture}>Tomar foto</Button>}
-              </Grid>
-
-
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Ingresar
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="/Register" variant="body2">
-                  No tienes cuenta? Registrate aqui!
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            ¿Querés crear tu cuenta?
+            <Link href="/Register" variant="body2">
+              Registrate aqui!
+            </Link>
+          </Typography>
         </Box>
+      </Modal>
 
-      </Container>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Login
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    type="email"
+                    label="Email"
+                    name="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    
+                    fullWidth
+                    name="password"
+                    label="Contraseña"
+                    type="password"
+                    id="password"
+                    autoComplete="new-password"
+                    onChange={(e)=>setBotton(e.nativeEvent.data==null?true:false)}
+/>
+                </Grid>
+                <Grid item xs={12}>
+                  <div style={{ display: 'flex', margin: '5px' }}>
+                    {(!imgSrc) ? <Webcam
+                      audio={false}
+                      screenshotFormat="image/jpeg"
+                      ref={webcamRef}
+                      videoConstraints={videoConstraints}
+                    >
+                    </Webcam> :
+                      (
+                        <img style={{ margin: '5px' }}
+                          src={imgSrc}
+                        />
+                      )
+                    }
+                  </div>
+
+                  {imgSrc ? <Button color={'error'} onClick={() => setImgSrc(null)} s startIcon={<DeleteIcon />}>
+                    Descartar foto
+                  </Button> : <Button onClick={capture}>Tomar foto</Button>}
+                </Grid>
+
+
+              </Grid>
+              <Button
+              disabled={botton}
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Ingresar
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="/Register" variant="body2">
+                    No tienes cuenta? Registrate aqui!
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+
+        </Container>
+      </ThemeProvider>
+    </div>
   );
 }
--- src/Page/Register.js
+++ src/Page/Register.js
@@ -15,7 +15,8 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Webcam from 'react-webcam';
 import { useCallback, useRef, useState } from "react";
-import { IconButton } from '@mui/material';
+import { IconButton, Modal } from '@mui/material';
+import { SignalCellularNoSimOutlined } from '@mui/icons-material';
 
 const theme = createTheme();
 
@@ -25,17 +26,35 @@ const videoConstraints = {
     margin: "5px"
 };
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
 
 export default function Register() {
 
     const [imgSrc, setImgSrc] = useState(null);
     const webcamRef = useRef(null);
     const [user, setUser] = useState(null);
+    const [botton, setBotton] = useState(true);
+    const [openModal, setOpenModal] = React.useState(false);
+
 
     const capture = useCallback(() => {
         const imageSrc = webcamRef.current.getScreenshot();
         setImgSrc(imageSrc);
+        setBotton(false)
     }, [webcamRef, setImgSrc]);
+
+
 
 
     const handleSubmit = (event) => {
@@ -54,9 +73,15 @@ export default function Register() {
             })
         };
         fetch('https://k9lcx9c6n9.execute-api.us-east-1.amazonaws.com/dev/user', requestOptions)
-            .then(response => response.json())
-            .then(data => setUser(data));
-
+            .then(response => {
+                if(response.status==403){
+                    setOpenModal(true)
+                    return;          
+                  }
+                  response.json().then((data) => {
+                    setUser(data); window.location.replace("/home")  
+                  })
+            })
     };
 
     if (user) {
@@ -65,107 +90,130 @@ export default function Register() {
 
 
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Typography component="h1" variant="h5">
-                        Crea tu cuenta
+        <>
+            <Modal
+                open={openModal}
+                onClose={() => setOpenModal(false)}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2">
+                    ¡Ya existe esta cuenta!
                     </Typography>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    autoComplete="given-name"
-                                    name="name"
-                                    fullWidth
-                                    id="name"
-                                    label="Nombre"
-                                    autoFocus
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-
-                                    fullWidth
-                                    id="lastName"
-                                    label="Apellido"
-                                    name="Last Name"
-                                    autoComplete="family-name"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Direccion de Email"
-                                    name="email"
-                                    autoComplete="email"
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-
-                                    fullWidth
-                                    name="password"
-                                    label="Contraseña"
-                                    type="password"
-                                    id="password"
-                                />
-                            </Grid>
-
-                            <Grid item xs={12}>
-                                <div style={{ display: 'flex', margin: '5px' }}>
-                                    {(!imgSrc) ? <Webcam
-                                        audio={false}
-                                        screenshotFormat="image/jpeg"
-                                        ref={webcamRef}
-                                        videoConstraints={videoConstraints}
-                                    >
-                                    </Webcam> :
-                                        (
-                                            <img style={{ margin: '5px' }}
-                                                src={imgSrc}
-                                            />
-                                        )
-                                    }
-                                </div>
-
-                                {imgSrc ? <Button color={'error'} onClick={() => setImgSrc(null)} s startIcon={<DeleteIcon />}>
-                                    Descartar foto
-                                </Button> : <Button onClick={capture}>Tomar foto</Button>}
-
-
-                            </Grid>
-
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Crear cuenta
-                        </Button>
-                        <Grid container justifyContent="flex-end">
-                            <Grid item>
-                                <Link href="/" variant="body2">
-                                    Ya tienes cuenta? Accede aqui!
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
+                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                        ¿Querés ingresar a tu cuenta?
+                        <Link href="/" variant="body2">
+                            Ingresa aqui
+                        </Link>
+                    </Typography>
                 </Box>
+            </Modal>
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Typography component="h1" variant="h5">
+                            Crea tu cuenta
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        autoComplete="given-name"
+                                        name="name"
+                                        fullWidth
+                                        id="name"
+                                        label="Nombre"
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
 
-            </Container>
-        </ThemeProvider>
+                                        fullWidth
+                                        id="lastName"
+                                        label="Apellido"
+                                        name="Last Name"
+                                        autoComplete="family-name"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        type="email"
+                                        label="Direccion de Email"
+                                        name="email"
+
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        o
+                                        fullWidth
+                                        name="password"
+                                        label="Contraseña"
+                                        type="password"
+                                        id="password"
+                                        onChange={(e) => setBotton(e.nativeEvent.data == null ? true : false)}
+                                    />
+                                </Grid>
+
+                                <Grid item xs={12}>
+                                    <div style={{ display: 'flex', margin: '5px' }}>
+                                        {(!imgSrc) ? <Webcam
+                                            audio={false}
+                                            screenshotFormat="image/jpeg"
+                                            ref={webcamRef}
+                                            videoConstraints={videoConstraints}
+                                        >
+                                        </Webcam> :
+                                            (
+                                                <img style={{ margin: '5px' }}
+                                                    src={imgSrc}
+                                                />
+                                            )
+                                        }
+                                    </div>
+
+                                    {imgSrc ? <Button color={'error'} onClick={() => { setImgSrc(null); setBotton(true) }} s startIcon={<DeleteIcon />}>
+                                        Descartar foto
+                                    </Button> : <Button onClick={capture}>Tomar foto</Button>}
+
+
+                                </Grid>
+
+                            </Grid>
+                            <Button
+                                disabled={botton}
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Crear cuenta
+                            </Button>
+                            <Grid container justifyContent="flex-end">
+                                <Grid item>
+                                    <Link href="/" variant="body2">
+                                        Ya tienes cuenta? Accede aqui!
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+
+                </Container>
+            </ThemeProvider>
+        </>
     );
 }
